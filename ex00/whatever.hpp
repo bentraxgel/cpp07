ca59acec2539e3112d1d351dac6631211d8511a1
seok <seok@student.42seoul.kr>
2024-02-15 08:56:03 +0900

    cpp07_retry

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -11,18 +11,9 @@ void	swap(T& x, T& y) {
 }
 
 template <typename T>
-T	max(T x, T y) {
-	if (x == y) {
-		return y;
-	}
-	return x > y? x : y;
-}
+T	max(T x, T y) { return x > y? x : y; }
+
 template <typename T>
-T	min(T x, T y) {
-	if (x == y) {
-		return y;
-	}
-	return x < y? x : y;
-}
+T	min(T x, T y) { return x < y? x : y; }
 
 #endif
