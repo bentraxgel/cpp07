0ae86d4b80d4a841aae7e23b4100d36ae4065f33
bentraxgel <92163446+bentraxgel@users.noreply.github.com>
2024-02-14 12:26:16 +0000

    cpp07_END

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -13,7 +13,6 @@ void	swap(T& x, T& y) {
 template <typename T>
 T	max(T x, T y) {
 	if (x == y) {
-		std::cout << "same: ";
 		return y;
 	}
 	return x > y? x : y;
@@ -21,7 +20,6 @@ T	max(T x, T y) {
 template <typename T>
 T	min(T x, T y) {
 	if (x == y) {
-		std::cout << "same: ";
 		return y;
 	}
 	return x < y? x : y;
