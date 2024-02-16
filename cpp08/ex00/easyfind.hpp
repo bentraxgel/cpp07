eab4927ae22dc1f8a50937bec798aecfe1a250ba
bentraxgel <dhrtpdnjs@gmail.com>
2024-02-16 20:13:14 +0900

    cpp08_ex00_macbook

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,7 +1,15 @@
-#pragma one
+#pragma once
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+
 template <typename T>
-easyfind(T, int);
+typename T::iterator easyfind(T& contaior, int n) {
+	typename T::iterator iter;
+	iter = std::find(contaior.begin(), contaior.end(), n);
+	if (iter == contaior.end())
+		throw std::exception();
+	return iter;
+}
 #endif
